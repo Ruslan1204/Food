916e0d0307f3a298c66767e690fd8ff48c13b613
Ruslan <ryslan12pess@ukr.net>
2023-12-26 19:31:28 +0200

    card

--- js/index.js
+++ js/index.js
@@ -190,32 +190,54 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container"
-  ).render();
+  const getResource = async (url) => {
+    const res = await fetch(url);
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-    14,
-    ".menu .container"
-  ).render();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+    return await res.json();
+  };
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню “Премиум”",
-    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-    21,
-    ".menu .container"
-  ).render();
+  getResource("http://localhost:3000/menu").then((data) => {
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu.container"
+      ).render();
+    });
+  });
+
+  // new MenuCard(
+  //   "img/tabs/vegy.jpg",
+  //   "vegy",
+  //   'Меню "Фитнес"',
+  //   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+  //   9,
+  //   ".menu .container"
+  // ).render();
+
+  // new MenuCard(
+  //   "img/tabs/post.jpg",
+  //   "post",
+  //   'Меню "Постное"',
+  //   "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+  //   14,
+  //   ".menu .container"
+  // ).render();
+
+  // new MenuCard(
+  //   "img/tabs/elite.jpg",
+  //   "elite",
+  //   "Меню “Премиум”",
+  //   "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+  //   21,
+  //   ".menu .container"
+  // ).render();
 
   // Forms
 
@@ -264,10 +286,12 @@ window.addEventListener("DOMContentLoaded", () => {
       //   "application/json; charset=utf-8"
       // );
 
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
+      // const object = {};
+      // formData.forEach(function (value, key) {
+      //   object[key] = value;
+      // });
+
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
       // const json = JSON.stringify(object);
 
@@ -280,7 +304,7 @@ window.addEventListener("DOMContentLoaded", () => {
       //   },
       //   body: JSON.stringify(object),
       // })
-      postData("http://localhost:3000/requests", JSON.stringify(object))
+      postData("http://localhost:3000/requests", json)
         // .then((data) => data.text())
         .then((data) => {
           console.log(data);
@@ -330,9 +354,9 @@ window.addEventListener("DOMContentLoaded", () => {
     }, 4000);
   }
 
-  fetch("http://localhost:3000/menu")
-    .then((data) => data.json())
-    .then((res) => console.log(res));
+  // fetch("http://localhost:3000/menu")
+  //   .then((data) => data.json())
+  //   .then((res) => console.log(res));
 });
 
 // fetch("https://jsonplaceholder.typicode.com/todos/1")
